feb294fd06d305efc2d3fdc31e470cec121a2fb9
Anteste <reversodr@gmail.com>
2020-06-21 16:26:04 +0100

    Game Ready

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NombreMystereConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomed = new Random().Next(20) + 1;
+
+            NouvellePartie(randomed);
+
+            YouWin(randomed);
+
+            Console.ReadKey();
+        }
+        static void NouvellePartie(int randomed)
+        {
+            Console.WriteLine("Saisissez un nombre entre 0 et 20: ");
+            int pickedNum = PickANumber();
+
+            while (pickedNum != randomed)
+            {
+                if (pickedNum > randomed)
+                {
+                    Console.WriteLine("C'est moins! ");
+                }
+
+                else
+                {
+                    Console.WriteLine("C'est plus!");
+                }
+                pickedNum = PickANumber();
+            }
+        }
+
+
+        static void YouWin(int randomed)
+        {
+            Console.WriteLine("Bravo ! Vous avez trouvez le nombre mystère : " + randomed);
+        }
+
+            static int PickANumber()
+        {
+            string picked = Console.ReadLine();
+
+            int pickedNum;
+
+            while (int.TryParse(picked, out pickedNum) == false)
+            {
+                Console.WriteLine("Oops Il ne s'agit pas d'un nombre. Essaie encore.");
+                picked = Console.ReadLine();
+            }
+            return pickedNum;
+
+            
+        }
+
+    }
+}
+
